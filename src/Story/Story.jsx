e402f7249f40ef56e255714e79d9eddb73670e58
sbaek0535 <baek27@wisc.edu>
2024-12-08 00:20:38 -0600

    Updated Story files as discussed in meeting

--- src/Story/Story.jsx
+++ src/Story/Story.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Story.css"; 
+import "./Story.css";
 import { FaUniversity, FaRocket, FaHandshake } from "react-icons/fa";
 import { Fade } from "react-awesome-reveal";
 
-const OurStory = () => {
+const Story = () => {
     return (
         <div className="our-story">
             <div className="our-story-content">
@@ -12,15 +12,15 @@ const OurStory = () => {
                     <div className="story-icons">
                         <div className="icon-box">
                             <FaUniversity className="icon" />
-                            <p>Prestigious Universities</p>
+                            <p className="icon-text">Prestigious Universities</p>
                         </div>
                         <div className="icon-box">
                             <FaRocket className="icon" />
-                            <p>Nationwide Expansion</p>
+                            <p className="icon-text">Nationwide Expansion</p>
                         </div>
                         <div className="icon-box">
                             <FaHandshake className="icon" />
-                            <p>Collaborative Partnerships</p>
+                            <p className="icon-text">Collaborative Partnerships</p>
                         </div>
                     </div>
                 </Fade>
