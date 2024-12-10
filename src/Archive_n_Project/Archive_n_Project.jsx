4097fc9813b6eb9d631c9ddc17c43364d333bf79
Rachel <cyoo7@wisc.edu>
2024-12-09 19:38:17 -0600

    Added css customization to archive&project section

--- src/Archive_n_Project/Archive_n_Project.jsx
+++ src/Archive_n_Project/Archive_n_Project.jsx
@@ -1,37 +1,78 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Archive_n_Project.css";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Archive_n_Project.css"; 
+
 
 import Image1 from "../Images/2024ProjectTeam.jpg";
 import Hackathon_Placeholder from "../Images/Hackathon_Placeholder.png";
-import Ideathon from "../Images/ideathon.png";
+import Ideathon from "../Images/ideathon_thumnail.jpeg";
+import CS_Team from "../Images/2024ProjectTeam.jpg";
+import DS_Team from "../Images/ideathon.png";
+
+const CustomPrevArrow = ({ onClick }) => (
+  <div
+    className="custom-arrow custom-prev-arrow"
+    onClick={onClick}
+    style={{ left: "-50px" }}
+  >
+    &#9664; {/* Left arrow symbol */}
+  </div>
+);
+
+const CustomNextArrow = ({ onClick }) => (
+  <div
+    className="custom-arrow custom-next-arrow"
+    onClick={onClick}
+    style={{ right: "-50px" }}
+  >
+    &#9654; {/* Right arrow symbol */}
+  </div>
+);
 
 const Archive_n_Project = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
+
+  const projects = [
+    { image: Image1, alt: "2024 Project Team", title: "2024 Project Team" },
+    { image: Hackathon_Placeholder, alt: "Hackathon 2025", title: "Hackathon 2025" },
+    { image: Ideathon, alt: "Ideathon 2024", title: "Ideathon 2024" },
+    { image: CS_Team, alt: "CS Team", title: "CS Team" },
+    { image: DS_Team, alt: "DS Team", title: "DS Team" },
+  ];
+
   return (
     <div className="archive">
       <h1>Archive & Projects</h1>
-      <p>Some projects and competition archives by our members</p>
-      <p>To see more detailed archives and other projects / competitions, click below:</p>
+      <p>Projects and competition archives of our members</p>
       
-      {/* Sample images */}
-      <div className="archive-images">
-        <figure className="archive-item">
-          <img src={Image1} alt="Our 2024 Project Team" className="archive-image" />
-          <figcaption>Our 2024 Project Team</figcaption>
-        </figure>
-        <figure className="archive-item">
-          <img src={Hackathon_Placeholder} alt="Our annual Hackathons" className="archive-image" />
-          <figcaption>Our Annual Hackathons</figcaption>
-        </figure>
-        <figure className="archive-item">
-          <img src={Ideathon} alt="Our annual ideathon projects" className="archive-image" />
-          <figcaption>Our Annual Ideathon Projects</figcaption>
-        </figure>
+      {/* Slider Section */}
+      <div className="archive-slider-container">
+        <Slider {...sliderSettings} className="archive-slider">
+          {projects.map((project, index) => (
+            <div key={index} className="slider-item">
+              <img src={project.image} alt={project.alt} className="slider-image" />
+              <p className="slider-caption">{project.title}</p>
+            </div>
+          ))}
+        </Slider>
       </div>
       
       {/* Link to archive & projects */}
       <Link to="/archive-projects" className="archivelink">
-        Archives & Projects
+        View All Archives & Projects
       </Link>
     </div>
   );
