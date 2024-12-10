e6ab3c328a91769378794036e43c2b32c7337a2e
Tyrrnien81 <june121284@gmail.com>
2024-12-10 07:38:08 -0600

    added link to apply button and fixed margin

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -24,36 +24,40 @@ const AboutUs = () => {
                             />
                         </h1>
                     </header>
-                    <div className="description">
-                        <img
-                            src={logo}
-                            alt="LikeLion UW-Madison Logo"
-                            className="logo-image"
-                        />
-                        <h2 className="slogan">
-                            <span className="slogan-word">Create</span>
-                            <span className="bullet">•</span>
-                            <span className="slogan-word">Collaborate</span>
-                            <span className="bullet">•</span>
-                            <span className="slogan-word">Conquer</span>
-                        </h2>
-                        <p className="main-text">
-                            LikeLion UW-Madison empowers individuals with
-                            resources, mentorship, and a collaborative
-                            environment to turn unique ideas into impactful
-                            realities. We foster creativity, teamwork, and the
-                            tools to bring your vision to life.
-                        </p>
-                        <p className="end-text">
-                            Let's bring your ideas to life, together!
-                        </p>
-                    </div>
-                    <div className="apply">
-                        <button className="apply-button">
-                            <span className="apply-word">Join Us</span>
-                        </button>
-                        <p className="apply-message">See you next round!</p>
-                    </div>
+                    <Fade direction="up" delay={100}>
+                        <div className="description">
+                            <img
+                                src={logo}
+                                alt="LikeLion UW-Madison Logo"
+                                className="logo-image"
+                            />
+                            <h2 className="slogan">
+                                <span className="slogan-word">Create</span>
+                                <span className="bullet">•</span>
+                                <span className="slogan-word">Collaborate</span>
+                                <span className="bullet">•</span>
+                                <span className="slogan-word">Conquer</span>
+                            </h2>
+                            <p className="main-text">
+                                LikeLion UW-Madison empowers individuals with
+                                resources, mentorship, and a collaborative
+                                environment to turn unique ideas into impactful
+                                realities. We foster creativity, teamwork, and
+                                the tools to bring your vision to life.
+                            </p>
+                            <p className="end-text">
+                                Let's bring your ideas to life, together!
+                            </p>
+                        </div>
+                        <div className="apply">
+                            <a href="https://www.instagram.com/likelion_wisconsin/">
+                                <button className="apply-button">
+                                    <span className="apply-word">Join Us</span>
+                                </button>
+                            </a>
+                            <p className="apply-message">See you next round!</p>
+                        </div>
+                    </Fade>
                 </Fade>
             </main>
         </div>
